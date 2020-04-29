44679510a7f8d42b6848618f7aaa03ee709306e2
nicholas570 <nicolas.mercier570@gmail.com>
2020-04-29 16:59:46 +0200

    getData algo done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import React from 'react';
 import RandomCards from './components/RandomCards';
-
-import axios from 'axios';
-
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      artPosts: [],
-    };
-    this.getArt = this.getArt.bind(this);
-  }
-
-  componentDidMount() {
-    this.getArt();
-  }
-
-  getArt() {
-    axios
-      .get(
-        'https://collectionapi.metmuseum.org/public/collection/v1/objects/465'
-      )
-      .then((res) =>
-        this.setState({ artPosts: res.data }, () =>
-          console.log(this.state.artPosts)
-        )
-      );
-  }
-
-  render() {
-    return (
-      <div className='App'>
-        {this.state.artPosts.objectID}
-        <RandomCards />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className='App'>
+      <RandomCards />
+    </div>
+  );
 }
 
 export default App;
--- src/components/RandomCards.jsx
+++ src/components/RandomCards.jsx
@@ -1,68 +1,89 @@
 import React from 'react';
+import axios from 'axios';
 
 import {
   Card,
   CardImg,
-  CardText,
   CardBody,
   CardTitle,
   CardSubtitle,
+  Button,
 } from 'reactstrap';
 
-const RandomCards = ({ image, title, artist, department, date, period }) => {
-  return (
-    <div>
-      <Card className='text-center' style={{ width: '65rem' }}>
-        <CardImg top width='100%' src={image} alt='Card image cap' />
+//const RandomCards = ({ image, title, artist, department, date, period }) => {
 
-        <CardBody>
-          <CardTitle>title</CardTitle>
-          <CardSubtitle>artist</CardSubtitle>
-          <CardSubtitle>department</CardSubtitle>
-          <CardSubtitle>date</CardSubtitle>
-          <CardSubtitle>period</CardSubtitle>
-          <CardText>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-            sit aspernatur provident voluptate in sequi mollitia incidunt dicta
-            quidem, error neque placeat consequatur magni, saepe ut harum. Quos,
-            accusamus doloribus.
-          </CardText>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+class RandomCards extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datas: {},
+      objectDisplayed: {},
+      isLoading: true,
+    };
+    this.getData = this.getData.bind(this);
+  }
+
+  getData() {
+    axios
+      .get(
+        'https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=1'
+      )
+      .then((res) => {
+        this.setState({ datas: res.data });
+        const randomId = this.randomId(this.state.datas.objectIDs);
+
+        axios
+          .get(
+            `https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomId}`
+          )
+          .then((res) => {
+            if (res.data.primaryImage === '') {
+              this.getData();
+            } else {
+              this.setState({ objectDisplayed: res.data, isLoading: false });
+            }
+          })
+          .catch((error) => {
+            console.log(error.response.status);
+            this.getData();
+          });
+      });
+  }
+
+  randomId(array) {
+    return Math.floor(Math.random() * Math.floor(array.length));
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <div>
+        <Card className='text-center' style={{ width: '65rem' }}>
+          <CardImg
+            top
+            width='100%'
+            src={this.state.objectDisplayed.primaryImage}
+            alt='Card image cap'
+          />
+
+          <CardBody>
+            <CardTitle>{this.state.objectDisplayed.title}</CardTitle>
+            <CardSubtitle>
+              {this.state.objectDisplayed.artistDisplayName}
+            </CardSubtitle>
+            <CardSubtitle>{this.state.objectDisplayed.department}</CardSubtitle>
+            <CardSubtitle>{this.state.objectDisplayed.objectDate}</CardSubtitle>
+          </CardBody>
+        </Card>
+        <Button color='info' onClick={this.getData}>
+          reload
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default RandomCards;
-
-//function RandomCards({ image, title, artist, department, date, period }) {
-//   return (
-//     <div className={styles.container}>
-//       <figure>
-//         <div>image</div>
-//         <figcaption>title</figcaption>
-//         <figcaption>artist</figcaption>
-//         <figcaption>department</figcaption>
-//         <figcaption>date</figcaption>
-//         <figcaption>period</figcaption>
-//         <figcaption>
-//           Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
-//           sit aspernatur provident voluptate in sequi mollitia incidunt dicta
-//           quidem, error neque placeat consequatur magni, saepe ut harum. Quos,
-//           accusamus doloribus.
-//         </figcaption>
-//       </figure>
-
-//       {/* <figure>
-//         {image}
-//         <figcaption>{title}</figcaption>
-//         <figcaption>{artist}</figcaption>
-//         <figcaption>{department}</figcaption>
-//         <figcaption>{date}</figcaption>
-//         <figcaption>{period}</figcaption>
-//       </figure> */}
-//     </div>
-//   );
-// }
-
-// export default RandomCards;
